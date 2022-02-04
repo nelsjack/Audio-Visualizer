b72c48ea6aead3adc005f58714a2b0fae91e5ca0
Jack Nelson <jacknelson@Jacks-MacBook-Air.local>
2022-02-04 16:27:30 -0500

    first commit

---
+++ src/index.js
@@ -0,0 +1,62 @@
+// Initializing an AudioContext to access Web Audio API methods
+const audioContext = new AudioContext();
+const watermelonManElement = document.querySelector('#watermelonman');
+const audioSource = audioContext.createMediaElementSource(watermelonManElement);
+
+// plug audio element into the AudionContext and connect output to speakers
+audioSource.connect(audioContext.destination);
+
+const playButtonNodeList = document.querySelectorAll("#playbutton");
+
+//look to add functionlity for  multiple tracks/play buttons, checking if button id === audio id (add playbutton class in HTML)
+playButtonNodeList.forEach((playButton) => {
+    playButton.addEventListener('click', () => {
+        if (!watermelonManElement.paused) {
+            watermelonManElement.pause();
+        } else if (watermelonManElement.paused) {
+            watermelonManElement.play(); 
+        }
+        startAudioContext();
+    });    
+});
+// AudioContext loads as suspended due to autoplay restrictions
+const startAudioContext = () => {
+    if (audioContext.state === 'suspended') {
+        audioContext.resume();
+        watermelonManElement.play();
+    }
+};
+
+// create analyser and print data onto the page
+// use analyser data to create canvas visualization
+const analyser = audioContext.createAnalyser();
+audioSource.connect(analyser);
+
+analyser.fftSize = 256;
+const bufferLength = analyser.frequencyBinCount;
+const dataArray = new Uint8Array(bufferLength);
+
+const canvas = document.querySelector('#frequency-canvas');
+const body = document.querySelector('body');
+const canvasContext = canvas.getContext('2d');
+
+const draw = () => {
+    requestAnimationFrame(draw);
+    analyser.getByteFrequencyData(dataArray);
+
+    canvasContext.fillStyle = 'rgb(54, 94, 70)';
+    canvasContext.fillRect(0, 0, canvas.width, canvas.height);
+
+    const barWidth = (canvas.width / (bufferLength / 4))
+    let xAxis = 0;
+
+    for (i = 0; i < bufferLength; i++) {
+        let barHeight = Math.pow(dataArray[i], 1.05);
+
+        canvasContext.fillStyle = 'rgb(' + barHeight + ',250 , 200)';
+        canvasContext.fillRect(xAxis, canvas.height-barHeight, barWidth, barHeight);
+
+        xAxis += barWidth + 1;
+    }
+};
+draw();
